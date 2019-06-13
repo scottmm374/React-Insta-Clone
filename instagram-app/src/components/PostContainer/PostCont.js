523c94b4dfd2d3aa325b853719f4a09828913754
Michelle Scott <scottmm374@gmail.com>
2019-06-12 21:26:40 -0400

    set up SearchBar component

--- instagram-app/src/components/PostContainer/PostCont.js
+++ instagram-app/src/components/PostContainer/PostCont.js
@@ -5,15 +5,14 @@ import CommentSection from "./CommentSection";
 function PostCont (props) {
     console.log(props)
     return (
-    
         <div className="comments">
         {props.instagramData.map(com => {
-            return <CommentSection post={com.comments}
+            return <CommentSection post={com.comments} />;
+        
+     })}
 
-        })}
-
-        </div>
-    )
+     </div>
+    );
 
 }
 
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import camera-logo from './assets/camera-logo.svg';
 
+function SearchBar () {
+    return (
+        <div>
+            <header>
+                <img src={camera-logo}/>
+                <img></img>
+                <input></input>
+                <img></img>
+                <img></img>
+                <img></img>
+            </header>
+        </div>
+    )
+}
+
+export default SearchBar;
